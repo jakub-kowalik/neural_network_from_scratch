869e374177be0fb47375032dd7a9bda9ffb2b130
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2022-12-14 19:20:39 +0100

    Added tanh and changed testing to tensorflow functions

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -1,7 +1,7 @@
-import math
-
 import numpy as np
 from abc import ABC, abstractmethod
+
+# @TODO tidy up this file
 
 
 class ActivationFunction(ABC):
@@ -26,12 +26,13 @@ class Sigmoid(ActivationFunction):
         return self._output
 
     def backward(self, x, *args):
-        self._error = 1 / (1 + np.exp(-self._output)) * 1 / (1 + np.exp(-(1 - self._output)))
+        self._error = (1 / (1 + np.exp(-self._output))) * (1 - (1 / (1 + np.exp(-self._output))))
         return self._error
 
 
-# https://stackoverflow.com/questions/32109319/how-to-implement-the-relu-function-in-numpy
+# @TODO make own implementation later
 class ReLU(ActivationFunction):
+    # https://stackoverflow.com/questions/32109319/how-to-implement-the-relu-function-in-numpy
     def forward(self, x):
         self._output = x * (x > 0)
         return self._output
@@ -43,21 +44,36 @@ class ReLU(ActivationFunction):
 
 class Softmax(ActivationFunction):
     # https://github.com/ddbourgin/numpy-ml/blob/master/numpy_ml/neural_nets/layers/layers.py#L2192-L2349
+    # probably more numerically stable than my previous implementation
     def forward(self, x):
-        # self._output = np.exp(x) / np.sum(np.exp(x))
-        # return self._output
-        e_X = np.exp(x - np.max(x, keepdims=True))
-        return e_X / e_X.sum(keepdims=True)
+        e_X = np.exp(x - np.max(x, axis=1, keepdims=True))
+        self._output = e_X / e_X.sum(axis=1, keepdims=True)
+        return self._output
 
     # https://math.stackexchange.com/questions/2843505/derivative-of-softmax-without-cross-entropy
+    # currently not being used anyway
     def backward(self, x):
-        return 1
-        s = self._output.reshape(-1, 1)
-        self._error = np.diagflat(s) - np.dot(s, s.T)
-        return self._error
+        return 1.0  # 1 to retain error on backpropagation multiply
+
+        # s = self._output.reshape(-1, 1)
+        # self._error = np.diagflat(s) - np.dot(s, s.T)
+        # return self._error
         # return np.exp(self._output) / np.sum(np.exp(self._output)) \
         #     * np.exp(1 - self._output) / np.sum(np.exp(1 - self._output))
 
         # self._error = self._output - x
         # return self._error
 
+
+class Tanh(ActivationFunction):
+    # https://pl.wikipedia.org/wiki/Funkcje_hiperboliczne
+    # apparently not numerically stable, might investigate later
+    # github copilot completion
+    # @TODO investigate numerically stable hyperbolic tangent
+    def forward(self, x):
+        self._output = np.tanh(x)
+        return self._output
+
+    def backward(self, x):
+        self._error = 1 - np.tanh(self._output) ** 2
+        return self._error
--- tests/test_activation_function.py
+++ tests/test_activation_function.py
@@ -1,13 +1,21 @@
-from src.activation_functions import Sigmoid, ReLU, Softmax
+from src.activation_functions import Sigmoid, ReLU, Softmax, Tanh
+
+import tensorflow as tf
 import scipy.special
 import numpy as np
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # suppress tensorflow warnings
 
 
 def test_sigmoid_forward():
     sigmoid = Sigmoid()
     random_array = np.random.rand(3, 3, 3)
 
-    assert np.isclose(sigmoid.forward(random_array), scipy.special.expit(random_array)).all()
+    assert np.isclose(
+        sigmoid.forward(random_array),
+        tf.nn.sigmoid(random_array)
+    ).all()
 
 
 def test_sigmoid_backward():
@@ -15,15 +23,20 @@ def test_sigmoid_backward():
     random_array = np.random.rand(3, 3, 3)
     sigmoid.forward(random_array)
 
-    assert np.isclose(sigmoid.backward(0),
-                      scipy.special.expit(sigmoid._output) * (scipy.special.expit(1 - sigmoid._output))).all()
+    assert np.isclose(
+        sigmoid.backward(0),
+        tf.nn.sigmoid(sigmoid._output) * (1 - tf.nn.sigmoid(sigmoid._output))
+    ).all()
 
 
 def test_relu_forward():
     relu = ReLU()
     random_array = np.random.rand(3, 3, 3)
 
-    assert np.all(relu.forward(random_array) == np.maximum(0, random_array))
+    assert np.isclose(
+        relu.forward(random_array),
+        tf.nn.relu(random_array)
+    ).all()
 
 
 def test_relu_backward():
@@ -31,19 +44,50 @@ def test_relu_backward():
     random_array = np.random.rand(3, 3, 3)
 
     relu.forward(random_array)
-    assert np.all(relu.backward(random_array) == np.where(np.maximum(0, random_array) > 0, 1, 0))
+    assert np.isclose(
+        relu.backward(random_array),
+        np.where(np.maximum(0, random_array) > 0, 1, 0)
+    ).all()
 
 
 def test_softmax_forward():
     softmax = Softmax()
     random_array = np.random.rand(3, 3, 3)
 
-    assert np.isclose(softmax.forward(random_array), scipy.special.softmax(random_array)).all()
+    assert np.isclose(
+        softmax.forward(random_array),
+        tf.nn.softmax(random_array, axis=1)  # axis 1 because of batching
+    ).all()
 
 
-# def test_softmax_backward():
-#     softmax = Softmax()
-#     random_array = np.random.rand(3, 3, 3)
-#
-#     assert np.isclose(softmax.forward(random_array), scipy.special.softmax(random_array)).all()
+def test_softmax_backward():
+    # should just return 1 in current implementation
+    softmax = Softmax()
+    random_array = np.random.rand(3, 3, 3)
+    softmax.forward(random_array)
 
+    assert np.isclose(
+        softmax.backward(random_array),
+        1
+    ).all()
+
+
+def test_tanh_forward():
+    tanh = Tanh()
+    random_array = np.random.rand(3, 3, 3)
+
+    assert np.isclose(
+        tanh.forward(random_array),
+        tf.nn.tanh(random_array)
+    ).all()
+
+
+def test_tanh_backward():
+    tanh = Tanh()
+    random_array = np.random.rand(3, 3, 3)
+    tanh.forward(random_array)
+
+    assert np.isclose(
+        tanh.backward(random_array),
+        1 - tf.nn.tanh(tanh._output) ** 2
+    ).all()
