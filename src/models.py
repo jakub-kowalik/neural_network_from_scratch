91f050b7c03a022578661050a366bd402e599255
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2022-12-14 19:22:32 +0100

    Slight change of api before overhaul

--- src/models.py
+++ src/models.py
@@ -1,15 +1,19 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from sklearn.utils import shuffle as sk_shuffle
+
 
 class Model(ABC):
     @abstractmethod
-    def __init__(self, epochs, learning_rate, loss_function, batch_size=1):
+    def __init__(self, epochs, learning_rate, loss_function, batch_size=1, verbose=False, shuffle=True):
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.loss_function = loss_function
-        if batch_size > 1:
-            print("Batch size is not supported yet")
-        self.batch_size = 1
+        # if batch_size > 1:
+        #     print("Batch size is not supported yet")
+        self.batch_size = batch_size
+        self.verbose = verbose
+        self.shuffle = shuffle
 
     @abstractmethod
     def predict(self, x):
@@ -21,8 +25,8 @@ class Model(ABC):
 
 
 class Sequential(Model):
-    def __init__(self, epochs, learning_rate, loss_function, batch_size=1):
-        super().__init__(epochs, learning_rate, loss_function, batch_size)
+    def __init__(self, epochs, learning_rate, loss_function, batch_size=1, verbose=False, shuffle=True):
+        super().__init__(epochs, learning_rate, loss_function, batch_size, verbose, shuffle)
         self.layers = []
 
     def add(self, layer):
@@ -34,8 +38,18 @@ class Sequential(Model):
             x = layer.forward(x)
         return x
 
+    def get_layer(self, index):
+        return self.layers[index]
+
     def train(self, x, y):
+        # @TODO later change params from model to this function
+        x = x.astype(np.float32)
+        y = y.astype(np.float32)
         for epoch in range(self.epochs):
+            # @TODO add own implementation
+            if self.shuffle:
+                x, y = sk_shuffle(x, y)
+
             for i in range(0, len(x), self.batch_size):
                 x_batch = x[i:i + self.batch_size]
                 y_batch = y[i:i + self.batch_size]
@@ -49,4 +63,5 @@ class Sequential(Model):
                     error = layer.backward(error, self.learning_rate)
 
             # if epoch % 100 == 0:
-            print(f"Epoch: {epoch}, Loss: {self.loss_function.forward(self.predict(x), y)}")
+            if self.verbose:
+                print(f"Epoch: {epoch}, Loss: {self.loss_function.forward(self.predict(x), y) / len(x)}")
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,28 +1,28 @@
 import numpy as np
 
 from src.layers import Linear
-from src.loss_functions import TestError
+from src.loss_functions import MeanAbsoluteError
 from src.models import Sequential
 
 
 def test_model_1_3_1():
     lin1 = Linear(n_inputs=1, n_outputs=3)
-    lin1.weights = np.asarray([[0.1, -0.1, 0.3]])
+    lin1.weights = np.asarray([[0.1, -0.1, 0.3]]).astype('float32')
 
     out1 = Linear(n_inputs=3, n_outputs=1)
-    out1.weights = np.asarray([[0.7], [0.9], [-0.4]])
+    out1.weights = np.asarray([[0.7], [0.9], [-0.4]]).astype('float32')
 
-    model = Sequential(epochs=1, learning_rate=0.01, loss_function=TestError())
+    model = Sequential(epochs=1, learning_rate=0.01, loss_function=MeanAbsoluteError(), shuffle=False)
 
     model.add(lin1)
     model.add(out1)
 
     model.train(np.asarray([[0.5]]), np.asarray([[0.1]]))
 
-    assert np.isclose(model.layers[0].weights,
+    assert np.isclose(model.get_layer(0).weights,
                       np.asarray([[0.100875, -0.1, 0.2995]]), rtol=1e-1).all()
 
-    assert np.isclose(model.layers[1].weights,
+    assert np.isclose(model.get_layer(1).weights,
                       np.asarray([[0.700125], [0.9], [-0.399625]]), rtol=1e-1).all()
 
 
@@ -33,16 +33,16 @@ def test_model_1_1_1():
     out1 = Linear(n_inputs=1, n_outputs=1)
     out1.weights = np.asarray([[0.3]]).astype('float32')
 
-    model = Sequential(epochs=1, learning_rate=0.01, loss_function=TestError())
+    model = Sequential(epochs=1, learning_rate=0.01, loss_function=MeanAbsoluteError(), shuffle=False)
 
     model.add(lin1)
     model.add(out1)
 
     model.train(np.asarray([[0.5]]), np.asarray([[0.1]]))
 
-    assert np.isclose(model.layers[0].weights,
+    assert np.isclose(model.get_layer(0).weights,
                       np.asarray([[0.100255]])).all()
 
-    assert np.isclose(model.layers[1].weights,
+    assert np.isclose(model.get_layer(1).weights,
                       np.asarray([[0.300085]])).all()
 
