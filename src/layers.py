f9df31e8a55694fb6e546e6ecdb90c47e6df6d1f
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2022-12-15 01:46:05 +0100

    working conv backward concept

--- src/layers.py
+++ src/layers.py
@@ -6,7 +6,6 @@ from src.activation_functions import ActivationFunction
 
 
 class Layer(ABC):
-
     _input = None
     _output = None
 
@@ -72,3 +71,85 @@ class Flatten(Layer):
 
     def backward(self, error, learning_rate):
         return error.reshape(self.input_shape)
+
+
+class Conv2D(Layer):
+    def __init__(self, n_filters, filter_size, stride=1, padding=0):
+        super().__init__()
+        self._input = None
+        self._output = None
+        self.n_filters = n_filters
+        self.filter_size = filter_size
+        self.stride = stride
+        self.padding = padding
+
+        stddev = np.sqrt(2 / (n_filters + filter_size + filter_size))
+        self.weights = np.random.normal(0, stddev, (n_filters, filter_size, filter_size)).astype(np.float32)
+        # self.weights = np.random.normal(0, 0.01, (n_filters, filter_size, filter_size)).astype(np.float32)
+        self.bias = np.zeros((1, n_filters)).astype(np.float32)
+
+    def forward(self, x):
+        self._input = x
+
+        # print(x.shape)
+       # padded\
+        padded = np.pad(x, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)))
+
+        batch_size, n_channels, height, width = padded.shape  # padded.shape
+
+        self._output = np.zeros((
+            batch_size,
+            self.n_filters,
+            ((x.shape[2] - self.filter_size + 2 * self.padding) // self.stride) + 1,
+            ((x.shape[2] - self.filter_size + 2 * self.padding) // self.stride) + 1
+        ))
+
+        out_samples, out_channels, out_height, out_width = self._output.shape
+
+        for b in range(out_samples):
+            for f in range(self.n_filters):
+                for h in range(out_height):
+                    for w in range(out_width):
+                        self._output[b, f, h, w] = np.sum(
+                            padded[
+                            b,
+                            :,
+                            h * self.stride:h * self.stride + self.filter_size,
+                            w * self.stride:w * self.stride + self.filter_size]
+                            *
+                            self.weights[f] + self.bias[0][f]  # no kernel flip np.flip()
+                        )
+
+        self._input = padded
+
+        return self._output
+
+    def backward(self, error, learning_rate):
+        batch_size, n_channels, height, width = self._input.shape
+        out_samples, out_channels, out_height, out_width = error.shape
+
+        error_padded = np.pad(error, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)))
+
+        input_error = np.zeros(error_padded.shape)
+        out_samples, out_channels, out_height, out_width = error.shape
+
+        for b in range(out_samples):
+            for f in range(self.n_filters):
+                for h in range(out_height):
+                    for w in range(out_width):
+                        input_error[
+                        b,
+                        f,
+                        h,
+                        w] = np.sum(error_padded[b, f, h:h + self.filter_size, w:w + self.filter_size]
+                                    * self.weights[f])
+
+        for f in range(self.n_filters):
+            for h in range(self.filter_size):
+                for w in range(self.filter_size):
+                    self.weights[:, h, w] -= learning_rate * np.sum(
+                        self._input[:, :, h:h + error.shape[-2], w:w + error.shape[-1]] * error)
+
+        self.bias -= learning_rate * np.sum(error)
+
+        return input_error
--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-from src.layers import Flatten
+from src.layers import Flatten, Conv2D
+
+import tensorflow as tf
 
 
 def test_flatten():
@@ -13,3 +15,111 @@ def test_flatten():
     assert forward_output.shape == (3, 20)
     assert backward_output.shape == random_array.shape
     assert np.isclose(random_array, backward_output).all()
+
+
+def test_conv2d_batch_controlled():
+    the_image = np.asarray([
+        [[
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]],
+        [[
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]],
+        [[
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]]
+    ])
+
+    filter_weights = np.asarray([
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1]
+    ])
+
+    my_conv = Conv2D(
+        n_filters=1,
+        filter_size=3,
+        stride=1,
+        padding=1
+    )
+
+    my_conv.weights = np.expand_dims(filter_weights, 0)
+
+    tf_conv2d = tf.nn.conv2d(
+        input=np.einsum("bchw -> bhwc", the_image),
+        filters=np.expand_dims(filter_weights, (2, 3)),
+        padding='SAME',
+        data_format='NHWC',
+        strides=1
+    )
+
+    assert np.isclose(
+        my_conv.forward(the_image),
+        np.einsum("NHWC->NCHW", tf_conv2d)
+    ).all()
+
+
+import numpy as np
+
+from src.layers import Flatten, Conv2D
+
+import tensorflow as tf
+
+
+def test_flatten():
+    flat = Flatten()
+    random_array = np.random.random((3, 4, 5))
+
+    forward_output = flat.forward(random_array)
+    backward_output = flat.backward(forward_output, 0)
+
+    assert forward_output.shape == (3, 20)
+    assert backward_output.shape == random_array.shape
+    assert np.isclose(random_array, backward_output).all()
+
+
+def test_conv2d_batch_random():
+    batch = np.random.randint(1, 100)
+    channels = 1  # @TODO figure out how tensorflow handles multiple channels in weights
+    height = np.random.randint(10, 100)
+    width = height
+    the_image = np.random.random((batch, channels, height, width)).astype(np.float32)
+
+    print(the_image.shape)
+
+    filter_size = np.random.randint(1, 10)
+    filter_weights = np.random.random((
+        filter_size,
+        filter_size
+    ))
+
+    my_conv = Conv2D(
+        n_filters=1,
+        filter_size=filter_size,
+        stride=1,
+        padding=0
+    )
+
+    my_conv.weights = np.expand_dims(filter_weights, 0).astype(np.float32)
+
+    tf_conv2d = tf.nn.conv2d(
+        input=np.einsum("bchw -> bhwc", the_image),
+        filters=np.expand_dims(filter_weights, (2, 3)),
+        padding='VALID',
+        data_format='NHWC',
+        strides=1
+    )
+    print(my_conv.forward(the_image).shape)
+    assert np.isclose(
+        my_conv.forward(the_image),
+        np.einsum("NHWC->NCHW", tf_conv2d)
+    ).all()
+
+
+
